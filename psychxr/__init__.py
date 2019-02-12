cd24a46577d4efb2bbcd155bee3f615e49e8fa9c
mdc <cutonem@yorku.ca>
2019-02-11 19:42:26 -0500

    DOCS: Updated docs.

--- psychxr/__init__.py
+++ psychxr/__init__.py
@@ -1,2 +1,45 @@
-__version__ = "0.1.4"
+#
+#  __init__.py
+#
+#  Copyright 2019 Matthew Cutone <cutonem(a)yorku.ca> and Laurie M. Wilcox
+#  <lmwilcox(a)yorku.ca>; The Centre For Vision Research, York University,
+#  Toronto, Canada
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+"""PsychXR is a collection of Python extension libraries for interacting with
+eXtended Reality displays (HMDs), intended for neuroscience and psychology
+research applications.
 
+PsychXR is used by PsychoPy v3 (http://www.psychopy.org/) to provide HMD
+support.
+
+"""
+# ------------------
+# Module information
+# ------------------
+#
+__author__ = "Matthew D. Cutone"
+__credits__ = ["Laurie M. Wilcox"]
+__copyright__ = "Copyright 2019 Matthew D. Cutone"
+__license__ = "MIT"
+__version__ = "0.2.0"
+__status__ = "Beta"
+__maintainer__ = "Matthew D. Cutone"
+__email__ = "cutonem@yorku.ca"
--- psychxr/libovr/__init__.py
+++ psychxr/libovr/__init__.py
@@ -1,2 +1,24 @@
-# load extension modules
+"""Interface and tools for the Oculus Rift head-mounted display (HMD) using the
+official Oculus PC SDK. The Oculus PC SDK is Copyright (c) Facebook
+Technologies, LLC and its affiliates. All rights reserved.
+
+"""
+# ------------------
+# Module information
+# ------------------
+#
+__author__ = "Matthew D. Cutone"
+__credits__ = ["Laurie M. Wilcox"]
+__copyright__ = "Copyright 2019 Matthew D. Cutone"
+__license__ = "MIT"
+__version__ = "0.2.0"
+__status__ = "Beta"
+__maintainer__ = "Matthew D. Cutone"
+__email__ = "cutonem@yorku.ca"
+
+# -------
+# Imports
+# -------
+#
+# load exported data from the _libovr extension module into the namespace
 from ._libovr import *
