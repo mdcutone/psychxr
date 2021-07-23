bc6556ef70c8b9f3fdab72f4efdfeefe344257be
Matthew D. Cutone <mcutone@opensciencetools.org>
2021-07-23 17:36:43 -0400

    NF: Added `OpenXRSystemInfo`

--- psychxr/drivers/openxr/_openxr.pyx
+++ psychxr/drivers/openxr/_openxr.pyx
@@ -42,8 +42,11 @@ __all__ = [
     'XR_SUCCESS',
     'XR_ERROR_HANDLE_INVALID',
     'XR_CURRENT_API_VERSION',
+    'XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY',
+    'XR_FORM_FACTOR_HANDHELD_DISPLAY',
+    'XR_NULL_SYSTEM_ID',
     'OpenXRApplicationInfo',
-    'OpenXRSystem',
+    'OpenXRSystemInfo',
     'createInstance',
     'hasInstance',
     'destroyInstance',
@@ -74,6 +77,8 @@ XR_ERROR_HANDLE_INVALID = openxr.XR_ERROR_HANDLE_INVALID
 XR_CURRENT_API_VERSION = openxr.XR_CURRENT_API_VERSION
 XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY = openxr.XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY
 XR_FORM_FACTOR_HANDHELD_DISPLAY = openxr.XR_FORM_FACTOR_HANDHELD_DISPLAY
+XR_NULL_SYSTEM_ID = openxr.XR_NULL_SYSTEM_ID
+XR_MIN_COMPOSITION_LAYERS_SUPPORTED = openxr.XR_MIN_COMPOSITION_LAYERS_SUPPORTED
 
 
 cdef char* str2bytes(str strIn):
@@ -95,6 +100,8 @@ cdef str bytes2str(char* bytesIn):
 
 class OpenXRError(BaseException):
     """Base exception for OpenXR related errors."""
+    def __init__(self, message):
+        self.message = message
 
 
 class OpenXRValidationFailureError(OpenXRError):
@@ -149,6 +156,14 @@ class OpenXRApiLayerNotPresentError(OpenXRError):
     pass
 
 
+class OpenXRFormFactorUnsupportedError(OpenXRError):
+    pass
+
+
+class OpenXRFormFactorUnavailableError(OpenXRError):
+    pass
+
+
 # lookup table of exceptions
 cdef dict openxr_error_lut = {
     openxr.XR_ERROR_VALIDATION_FAILURE: OpenXRValidationFailureError,
@@ -158,12 +173,14 @@ cdef dict openxr_error_lut = {
     openxr.XR_ERROR_INSTANCE_LOST: OpenXRInstanceLostError,
     openxr.XR_ERROR_SYSTEM_INVALID: OpenXRSystemInvalidError,
     openxr.XR_ERROR_LIMIT_REACHED: OpenXRLimitReachedError,
-    openxr.XR_ERROR_RUNTIME_UNAVAILABLE : OpenXRRuntimeUnavailableError,
-    openxr.XR_ERROR_NAME_INVALID : OpenXRNameInvalidError,
-    openxr.XR_ERROR_INITIALIZATION_FAILED : OpenXRNameInitializationFailedError,
-    openxr.XR_ERROR_EXTENSION_NOT_PRESENT : OpenXRNameExtensionNotPresentError,
-    openxr.XR_ERROR_API_VERSION_UNSUPPORTED : OpenXRApiVersionNotSupportedError,
-    openxr.XR_ERROR_API_LAYER_NOT_PRESENT : OpenXRApiLayerNotPresentError
+    openxr.XR_ERROR_RUNTIME_UNAVAILABLE: OpenXRRuntimeUnavailableError,
+    openxr.XR_ERROR_NAME_INVALID: OpenXRNameInvalidError,
+    openxr.XR_ERROR_INITIALIZATION_FAILED: OpenXRNameInitializationFailedError,
+    openxr.XR_ERROR_EXTENSION_NOT_PRESENT: OpenXRNameExtensionNotPresentError,
+    openxr.XR_ERROR_API_VERSION_UNSUPPORTED: OpenXRApiVersionNotSupportedError,
+    openxr.XR_ERROR_API_LAYER_NOT_PRESENT: OpenXRApiLayerNotPresentError,
+    openxr.XR_ERROR_FORM_FACTOR_UNSUPPORTED: OpenXRFormFactorUnsupportedError,
+    openxr.XR_ERROR_FORM_FACTOR_UNAVAILABLE: OpenXRFormFactorUnavailableError
 }
 
 
@@ -212,7 +229,9 @@ cdef class OpenXRApplicationInfo:
         self.apiVersion = apiVersion
 
     def __repr__(self):
-        return (f"XrApplicationInfo(applicationName='{self.applicationName}', "
+        return (
+            f"OpenXRApplicationInfo("
+            f"applicationName='{self.applicationName}', "
             f"applicationVersion={self.applicationVersion}, "
             f"engineName='{self.engineName}', "
             f"engineVersion={self.engineVersion}, "
@@ -308,33 +327,50 @@ cdef class OpenXRApplicationInfo:
         self.c_data.apiVersion = <openxr.XrVersion>value
 
 
-cdef class OpenXRSystem:
-    """Descriptor for a system (HMD, controllers, etc.) available to OpenXR.
+cdef class OpenXRSystemInfo:
+    """Descriptor for a system (head-mounted or handheld, etc.) available to
+    OpenXR.
 
-    These are instanced when calling :func:`getSystem`, users should not
-    instance this themselves during regular use.
+    These are instanced and returned when calling :func:`getSystem`, users
+    should not instance this themselves during regular use.
 
     """
     cdef openxr.XrSystemProperties c_data
 
     def __init__(self,
-                 systemId=0,
+                 systemId=XR_NULL_SYSTEM_ID,
                  vendorId=0,
-                 systemName=""):
+                 systemName="",
+                 maxSwapchainImageHeight=0,
+                 maxSwapchainImageWidth=0,
+                 maxLayerCount=XR_MIN_COMPOSITION_LAYERS_SUPPORTED,
+                 orientationTracking=False,
+                 positionTracking=False):
 
         self.systemId = systemId
         self.vendorId = vendorId
         self.systemName = systemName
-        # self.graphicsProperties = graphicsProperties
-        # self.trackingProperties = trackingProperties
+        self.maxSwapchainImageHeight = maxSwapchainImageHeight
+        self.maxSwapchainImageWidth = maxSwapchainImageWidth
+        self.maxLayerCount = maxLayerCount
+        self.orientationTracking = orientationTracking
+        self.positionTracking = positionTracking
 
     def __cinit__(self, *args, **kwargs):
         self.c_data.type = openxr.XR_TYPE_SYSTEM_PROPERTIES
 
     def __repr__(self):
-        return (f"OpenXRSystem(systemId='{self.systemId}', "
+        return (
+            f"OpenXRSystem("
+            f"systemId={self.systemId}, "
             f"vendorId={self.vendorId}, "
-            f"engineName='{self.systemName}')")
+            f"systemName='{self.systemName}', "
+            f"maxSwapchainImageHeight={self.maxSwapchainImageHeight}, "
+            f"maxSwapchainImageWidth={self.maxSwapchainImageWidth}, "
+            f"maxLayerCount={self.maxLayerCount}, "
+            f"orientationTracking={self.orientationTracking}, "
+            f"positionTracking={self.positionTracking})"
+        )
 
     @property
     def systemId(self):
@@ -382,6 +418,98 @@ cdef class OpenXRSystem:
                 'length <{}'.format(openxr.XR_MAX_SYSTEM_NAME_SIZE))
 
         self.c_data.systemName = str2bytes(value)
+
+    @property
+    def maxSwapchainImageHeight(self):
+        """Maximum swap chain image height in pixels (`int`)."""
+        return self.c_data.graphicsProperties.maxSwapchainImageHeight
+
+    @maxSwapchainImageHeight.setter
+    def maxSwapchainImageHeight(self, value):
+        if not isinstance(value, int):  # check type
+            raise TypeError(
+                'Property `OpenXRSystem.maxSwapchainImageHeight` must be type '
+                '`int`.')
+
+        self.c_data.graphicsProperties.maxSwapchainImageHeight = <int>value
+
+    @property
+    def maxSwapchainImageWidth(self):
+        """Maximum supported swap chain image width in pixels (`int`)."""
+        return self.c_data.graphicsProperties.maxSwapchainImageHeight
+
+    @maxSwapchainImageWidth.setter
+    def maxSwapchainImageWidth(self, value):
+        if not isinstance(value, int):  # check type
+            raise TypeError(
+                'Property `OpenXRSystem.maxSwapchainImageWidth` must be type '
+                '`int`.')
+
+        self.c_data.graphicsProperties.maxSwapchainImageWidth = <int>value
+
+    @property
+    def maxSwapchainSize(self):
+        """Maximum supported width and height of a swapchain image in pixels
+        (`tuple`)."""
+        return self.maxSwapchainImageWidth, self.maxSwapchainImageHeight
+
+    @property
+    def maxLayerCount(self):
+        """Maximum number of layers allowed (`int`)."""
+        return self.c_data.graphicsProperties.maxLayerCount
+
+    @maxLayerCount.setter
+    def maxLayerCount(self, value):
+        if not isinstance(value, int):  # check type
+            raise TypeError(
+                'Property `OpenXRSystem.maxLayerCount` must be type '
+                '`int`.')
+
+        if value < openxr.XR_MIN_COMPOSITION_LAYERS_SUPPORTED:
+            raise ValueError(
+                'Property `OpenXRSystem.maxLayerCount` must be greater than '
+                '`XR_MIN_COMPOSITION_LAYERS_SUPPORTED`.'
+            )
+
+        self.c_data.graphicsProperties.maxLayerCount = <int>value
+
+    @property
+    def orientationTracking(self):
+        """`True` if the system is capable of tracking orientation (`bool`)."""
+        return <bint>self.c_data.trackingProperties.orientationTracking
+
+    @orientationTracking.setter
+    def orientationTracking(self, value):
+        if not isinstance(value, (bool, int)):  # check type, ints are okay
+            raise TypeError(
+                'Property `OpenXRSystem.orientationTracking` must be type '
+                '`bool`.'
+            )
+
+        self.c_data.trackingProperties.orientationTracking = \
+            <openxr.XrBool32>value
+
+    @property
+    def positionTracking(self):
+        """`True` if the system is capable of tracking position (`bool`)."""
+        return <bint>self.c_data.trackingProperties.positionTracking
+
+    @positionTracking.setter
+    def positionTracking(self, value):
+        if not isinstance(value, (bool, int)):  # check type, ints are okay
+            raise TypeError(
+                'Property `OpenXRSystem.positionTracking` must be type '
+                '`bool`.'
+            )
+
+        self.c_data.trackingProperties.positionTracking = <openxr.XrBool32>value
+
+    @property
+    def fullTracking(self):
+        """`True` if this system has both orientation and position tracking
+        capabilities (`bool`).
+        """
+        return self.positionTracking and self.orientationTracking
 
 
 def createInstance(OpenXRApplicationInfo applicationInfo):
@@ -490,32 +618,51 @@ def getSystem(formFactor):
 
     Returns
     -------
-    tuple
-        API call result for `xrGetSystem` (`int`) and the enumerated system ID
-        (`int`) matching the desired `formFactor`. If no system was found, the
-        second value will be `None`.
+    OpenXRSystemInfo
+        Descriptor containing information about the system.
 
     Examples
     --------
     Get a system ID handle for an OpenXR supported HMD::
 
-        result, system_id =
+        try:
+            system_id = getSystem(XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY)
+        except (OpenXRFormFactorUnsupportedError, OpenXRFormFactorUnavailableError):
+            print('Specified form factor cannot be used.')
 
     """
     global _ptrInstance
     cdef openxr.XrSystemId system_id
+    cdef openxr.XrResult result
     cdef openxr.XrSystemGetInfo system_get_info
 
     # set the form factor to get
+    system_get_info.type = openxr.XR_TYPE_SYSTEM_GET_INFO
     system_get_info.formFactor = formFactor
+    system_get_info.next = NULL
 
     # get the system
-    cdef openxr.XrResult result = openxr.xrGetSystem(
+    result = openxr.xrGetSystem(
         _ptrInstance,
         &system_get_info,
         &system_id)
 
-    if result == openxr.XR_SUCCESS:  # failed to get a system, return nothing
-        return system_id
-    else:
+    if result < openxr.XR_SUCCESS:  # failed to get a system, return nothing
         raise openxr_error_lut[result]()
+
+    # get system properties
+    cdef openxr.XrSystemProperties system_props
+    system_props.type = openxr.XR_TYPE_SYSTEM_PROPERTIES
+    system_props.next = NULL
+    result = openxr.xrGetSystemProperties(
+        _ptrInstance,
+        system_id,
+        &system_props)
+
+    if result < openxr.XR_SUCCESS:
+        raise openxr_error_lut[result]()
+
+    cdef OpenXRSystemInfo system_desc = OpenXRSystemInfo()
+    system_desc.c_data = system_props
+
+    return system_desc
